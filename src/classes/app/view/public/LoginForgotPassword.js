2aab6e49e8df8abfa8cfeff942a9c2ba204fd31b
Bernard Bussy <brbitzbussy@gmail.com>
2019-07-14 10:48:49 +0200

    add click around on login forgot password view card tests

--- src/classes/app/view/public/LoginForgotPassword.js
+++ src/classes/app/view/public/LoginForgotPassword.js
@@ -4,17 +4,26 @@ class LoginForgotPassword extends ViewBase {
   async initialise(timeout = 4000) {
     await super.initialise(timeout)
 
+    // click through forgot password states
+    await this.click(LoginForgotPassword.forgotPasswordLabelID)
+    await this.waitForVisible(LoginForgotPassword.returnToLoginCardLabelID)
+    await this.click(LoginForgotPassword.returnToLoginCardLabelID)
+    await super.initialise(timeout)
   }
 }
 
+LoginForgotPassword.loginCardRootID = '#loginCardRoot'
 LoginForgotPassword.usernameOrEmailAddressInputID = '#usernameOrEmailAddress'
 LoginForgotPassword.passwordInputID = '#password'
 LoginForgotPassword.loginButtonID = '#loginButton'
 LoginForgotPassword.forgotPasswordLabelID = '#forgotPasswordLabel'
 
+LoginForgotPassword.forgotPasswordCaptchaCardRootID = '#forgotPasswordCaptchaCardRoot'
+LoginForgotPassword.returnToLoginCardLabelID = '#returnToLoginCardLabel'
+
 LoginForgotPassword.initialSelectors = [
   '#loginForgotPasswordRoot',
-  '#loginCardRoot',
+  LoginForgotPassword.loginCardRootID,
   LoginForgotPassword.usernameOrEmailAddressInputID,
   LoginForgotPassword.passwordInputID,
   LoginForgotPassword.loginButtonID,
